ef94a4db233a634ad052c911d925f524726cd3e0
Automated Commit <admin@vooward.com>
2025-11-19 19:57:04 +0300

    Remove duplicate backend directories and redundant configuration files

--- backend/backend/src/lib/validators.js
+++
@@ -1,6 +0,0 @@
-﻿const { z } = require('zod');
-const UssdBodySchema = z.object({
-  sessionId: z.string().min(1),
-  phoneNumber: z.string().regex(/^\+?[0-9]{10,13}$/)
-});
-module.exports = { UssdBodySchema };
